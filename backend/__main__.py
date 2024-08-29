944cb8663e8d0aa78f7f072caf23b02d70a8ed05
Renks <59278105+Renks@users.noreply.github.com>
2024-08-29 18:51:35 +0530

    ngrok add

--- backend/__main__.py
+++ backend/__main__.py
@@ -5,6 +5,12 @@ os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
 import time
 import uvicorn
+import nest_asyncio
+from pyngrok import ngrok
+
+ngrok_tunnel = ngrok.connect(8000)
+print('Public URL:', ngrok_tunnel.public_url)
+nest_asyncio.apply()
 
 start_time = time.perf_counter()
 from .main import app
@@ -14,4 +20,7 @@ elapsed_time = end_time - start_time
 
 print(f"Startup in {elapsed_time:.6f} seconds")
 
+ngrok_tunnel = ngrok.connect(8000)
+print('Public URL:', ngrok_tunnel.public_url)
+nest_asyncio.apply()
 uvicorn.run(app, host="0.0.0.0", port=8000)
